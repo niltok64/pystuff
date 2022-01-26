1f1f2eb93ac05e0e16b7a77e7a98a64f81630dd7
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-26 12:02:22 +0000

    Update hexagon.py

--- bestagon/hexagon.py
+++ bestagon/hexagon.py
@@ -4,3 +4,12 @@ def hexagon(size):
   for i in range(6):
     forward(size)
     left(60)
+
+hexagon(100)
+x = -10
+y = -30
+penup()
+goto(x, y)
+pendown()
+style = ('Courier', 10, 'italic')
+write("hexagons are the bestagons", font=style)
