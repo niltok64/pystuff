8c9518965ef5dc70726ce09745bdef8110f585e6
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-26 17:02:45 +0000

    Create windows_build_all.bat

---
+++ builders/windows_build_all.bat
@@ -0,0 +1,20 @@
+@echo off
+pip install --upgrade pip
+pip install pyinstaller
+cd ..
+cd bestagon
+pyinstaller hexagon.py --onefile
+cd ..
+cd pewpew
+pyinstaller pew.py --onefile
+cd ..
+cd trianglehaha
+pyinstaller main.py --onefile --add-data triangle.py
+cd ..
+cd twoturtles
+pyinstaller two.py --onefile
+cd ..
+cd windowssquare
+pyinstaller lol.py --onefile
+echo done building all programs, check respective "dist/" folders for executable
+pause
